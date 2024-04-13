9a8ea2bfbe6ca54237214106dd0a1d243651731f
benjamin <benjamin.bettan@gmail.com>
2024-04-13 23:59:14 +0200

    Changement de contexte : JdR Personnages + Armes

--- test.py
+++ test.py
@@ -1,44 +1,56 @@
 import json
 
-class PhoneNumber:
-    def __init__(self, phone_type, number):
-        self.phone_type = phone_type
-        self.number = number
-
-    def display_details1(self):
-        print(f"Type: {self.phone_type}, Number: {self.number}")
-
 class Person:
-    def __init__(self, name, age, city, phone_numbers):
+    def __init__(self, name, age, city, weapon):
         self.name = name
         self.age = age
         self.city = city
-        self.phone_numbers = [PhoneNumber(p['type'], p['number']) for p in phone_numbers]
+        self.weapon = weapon
 
-    def display_details2(self):
-        print(f"Name: {self.name}, Age: {self.age}, City: {self.city}")
-        if self.phone_numbers:
-            print("Phone Numbers:")
-            for phone_number in self.phone_numbers:
-                phone_number.display_details1()
-        else:
-            print("No phone numbers")
+    def display_details_person(self):
+        print(f"Name: {self.name}, Age: {self.age}, City: {self.city}, Weapon: {self.weapon}")
+
+class Weapon:
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+
+    def display_details_weapon(self):
+        print(f"Name: {self.name}, Level: {self.level}")
+
+
 
 def create_person_objects_from_json(json_file):
     with open(json_file, 'r') as file:
         data = json.load(file)
         persons = []
         for item in data:
-            person = Person(item['name'], item['age'], item['city'], item['phone_numbers'])
+            person = Person(item['name'], item['age'], item['city'], item['weapon'])
             persons.append(person)
         return persons
 
+def create_weapons_objects_from_json(json_file):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+        weapons = []
+        for item in data:
+            weapon = Weapon(item['name'], item['level'])
+            weapons.append(weapon)
+        return weapons
+
 def main():
-    json_file = 'json.json'  # Chemin vers votre fichier JSON
+    json_file = 'personnages.json'  # Chemin vers votre fichier JSON
     persons = create_person_objects_from_json(json_file)
 
     for person in persons:
-        person.display_details2()
+        person.display_details_person()
+        print()  # Ajoute une ligne vide entre chaque personne
+
+    json_file = 'armes.json'  # Chemin vers votre fichier JSON
+    weapons = create_weapons_objects_from_json(json_file)
+
+    for weapon in weapons:
+        weapon.display_details_weapon()
         print()  # Ajoute une ligne vide entre chaque personne
 
 if __name__ == "__main__":
